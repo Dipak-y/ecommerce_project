0a499440622937365da4cc42e4f0b223e08dfd86
Dipak Kumar Yadav <ydipakkumar360@gmail.com>
2026-09-07 13:15:24 +0545

    Configure PostgreSQL for Vercel deployment

--- merokart/settings.py
+++ merokart/settings.py
@@ -78,13 +78,20 @@ WSGI_APPLICATION = "merokart.wsgi.application"
 
 
 # Database
+
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
+if DATABASES["default"]["ENGINE"] != "django.db.backends.sqlite3":
+    DATABASES["default"]["OPTIONS"] = {
+        "sslmode": "require",
+    }
 
 # Authentication
 LOGIN_URL = "accounts:login"
